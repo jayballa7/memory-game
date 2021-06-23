d7ac5a606d2f874d76b9db45a506affa4d5f735d
Jenn Ballard <jenn_ballard7@hotmail.com>
2021-06-23 01:53:51 -0700

    pic changes at index of click

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,15 +1,98 @@
-import React from 'react';
+// import React, {useState} from 'react';
+// import './styles.css';
+// import Tile from '../Tile/index.js';
+
+// function Game () {
+
+//     let [tileCards, setTileCards] = useState([<Tile id = {0}/>, <Tile id = {1}/>, <Tile id = {2}/>, <Tile id = {3}/>, <Tile id = {4}/>, <Tile id = {5}/>, <Tile id = {6}/>,<Tile id = {7}/>, <Tile id = {8}/>, <Tile id = {9}/>, <Tile id = {10}/>, <Tile id = {11}/>, <Tile id = {12}/>, <Tile id = {13}/>, <Tile id = {14}/>, <Tile id = {15}/>, <Tile id = {16}/>, <Tile id = {17}/>, <Tile id = {18}/>, <Tile id = {19}/>]);
+
+
+//     return (
+//         <div className = "game-wrapper">
+//             <div className = "tiles-div">
+//                 {tileCards}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default Game;
+
+import React, {useState} from 'react';
+import update from 'immutability-helper';
 import './styles.css';
 import Tile from '../Tile/index.js';
+import faces from '../Faces/index.js';
+import controller from '../../img/controller.png';
+import donkeykong from '../../img/donkeykong.jpg';
 
 function Game () {
 
-    var tileCards = [];
+    let characters = faces;
+    
+    const cardClick = (id) => {
+        let idx = id;
+        let newDeck = [...tileCards];
+        newDeck[idx] = <Tile image = {donkeykong} />;
+        setTileCards(newDeck);
+    }
 
-        for(var x = 0; x < 20; x++) {
-            tileCards.push(<Tile key={x} id = {x} />);
-        }
+    let initialState = [
+        <Tile key = {0} id = {0} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {1} id = {1} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {2} id = {2} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {3} id = {3} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {4} id = {4} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {5} id = {5} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {6} id = {6} image = {controller} cardClick = {cardClick}/>,
+        <Tile key = {7} id = {7} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {8} id = {8} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {9} id = {9} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {10} id = {10} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {11} id = {11} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {12} id = {12} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {13} id = {13} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {14} id = {14} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {15} id = {15} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {16} id = {16} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {17} id = {17} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {18} id = {18} image = {controller} cardClick = {cardClick}/>, 
+        <Tile key = {19} id = {19} image = {controller} cardClick = {cardClick}/>
+    ]
 
+    let [tileCards, setTileCards] = useState(initialState);
+
+    // console.log(tileCards[0].props.image);
+    // console.log(tileCards[1].props.id);
+
+    // const updateImage = (index) => {
+    //     // setTileCards(<Tile id = {0} image = {faces[0]} />)
+    //     let newDeck = [...tileCards]; // copying the old datas array
+    //     newDeck[index] = <Tile image = {characters[0]} />; // replace e.target.value with whatever you want to change it to
+    //     setTileCards(newDeck); 
+    //     console.log(newDeck);
+    // }
+
+    // let updateImage = () => {
+    //     let index = 0;
+    //     let newDeck = [...tileCards];
+    //     newDeck[index] = <Tile image = {donkeykong} />
+    //     setTileCards(newDeck)
+
+        // let index = 0;
+        // let newDeck = update(tileCards, {
+           
+             
+        //       [index]: {
+                      
+        //         image: { $set: donkeykong}
+        //        }
+            
+        
+        // });
+        // setTileCards(newDeck);
+
+    //   }
 
     return (
         <div className = "game-wrapper">
@@ -21,191 +104,3 @@ function Game () {
 }
 
 export default Game;
-
-// import React, {Component} from 'react';
-// import './styles.css';
-// import update from 'immutability-helper';
-// import Tile from '../Tile/index.js';
-// import faces from '../Faces/index.js';
-// import deck from '../Deck/index.js';
-
-// class Game extends Component {
-    
-// constructor(props) {
-
-    
-//     super(props);
-//     this.state = {
-//         highScore: 0,
-//         currentScore: 0,
-//         Clicked: false,
-//     };
-
-//     this.handleClick = this.handleClick.bind(this);
-//     // this.createDeck = this.createDeck.bind(this);
-// };
-
-
-// // createDeck = function() {
-// //     for(var i = 0; i < 20; i++) {
-// //         var obj = {
-// //             "id": i,
-// //             image: isFaceDown ? controller : donkeykong
-// //         };
-// //         deck.push(obj);
-// //     };
-// // };
-
-//     //   updateObjectInArray = (array, action) => {
-//     //     return array.map((item, index) => {
-//     //       if (index !== action.index) {
-//     //         // This isn't the item we care about - keep it as-is
-//     //         return item
-//     //       }
-      
-//     //       // Otherwise, this is the one we want - return an updated value
-//     //       return {
-//     //         ...item,
-//     //         ...action.item
-//     //       }
-//     //     })
-//     //   }
-
-//     //   handleClick = id => {
-//     //     let updatedArr = this.updateObjectInArray(deck);
-//     //     this.setState({ updatedArr });
-//     //   };
-
-
-//         // handleClick = (id) => {
-//         //     var newDeck = this.state.deck[id];
-//         //     var chars = this.state.faces;
-//         //     var updatedDeck = update(newDeck, {$set: chars}); 
-//         //     this.setState({deck: updatedDeck});
-//         // }
-
-//         // handleClick = (id, image) => {
-//         //     var char = this.state.faces;
-//         //     this.setState({
-//         //         deck: this.state.deck.map(el => (el.id === id ? Object.assign({}, el, { image }) : char.image))
-//         //       });
-//         // }
-
-//         // handleUpdate = (todo) => {
-//         //     setTodos({...todos, [todo.id]: todo});
-//         //   }
-
-//         handleClick = (id) => {
-//             this.setState({isFaceDown: false});
-//           }
-
-    
-// //     shuffleArray = () => {
-// //         // Shuffle array of objects
-// //         const shuffledArr = this.shuffle(this.state.deck);
-// //         // Setting 'shuffledArr' as the new state
-// //         this.setState({ shuffledArr });
-// //     };
-
-// //       // handleIncrement increments this.state.currentScore by 1
-// //   handleIncrement = () => {
-// //     // Using setState method to update component's state
-// //     this.setState({ currentScore: this.state.currentScore + 1 });
-// //   };
-
-// //       // Function that takes an array as a parameter and shuffles it
-// //   shuffle = array => {
-// //     var currentIndex = array.length,
-// //       temporaryValue,
-// //       randomIndex;
-
-// //     // While there remain elements to shuffle...
-// //     while (0 !== currentIndex) {
-// //       // Pick a remaining element...
-// //       randomIndex = Math.floor(Math.random() * currentIndex);
-// //       currentIndex -= 1;
-
-// //       // And swap it with the current element.
-// //       temporaryValue = array[currentIndex];
-// //       array[currentIndex] = array[randomIndex];
-// //       array[randomIndex] = temporaryValue;
-// //     }
-// //     return array;
-// //   };
-    
-
-// //     handleClick = id => {
-// //         this.setState(prevState => ({
-        
-// //           faces: prevState.faces.map(
-// //             el => el.id === id? { ...el, image: controller }: el
-// //           )
-        
-// //         }))
-// //       };
-
-//     //   handleScore = id => {
-//     //     this.state.faces.forEach(element => {
-//     //       if (id === element.id && element.clicked === false) {
-//     //         element.clicked = true;
-//     //         this.setState({ Clicked: false });
-//     //         this.handleIncrement();
-//     //       } else if (id === element.id && element.clicked === true) {
-//     //         if (this.state.currentScore > this.state.highScore) {
-//     //           this.setState({ highScore: this.state.currentScore });
-//     //         }
-//     //         this.setState({ currentScore: 0 });
-//     //         this.setState({ Clicked: true });
-//     //         this.state.faces.forEach(element => (element.clicked = false));
-//     //         console.log(this.state.faces);
-//     //       }
-//     //     });
-//     //   };
-
-//     render() {
-
-
-//         return(
-//             <div className = "game-wrapper">
-//                 <div className = "tiles-div">
-//                     {/* {this.state.deck.map((keyName, i) => ( */}
-//                                 {/* <Tile 
-//                                     // Clicked={this.state.Clicked}
-//                                     handleClick={this.handleClick}
-//                                     // key={i}
-//                                     // image={keyName.image}
-//                                 /> */}
-//                     {numTiles.map(i => {
-//                         return <Tile />
-//                     })}
-//                     {/* ))} */}
-//                 </div>
-//             </div>
-//         )           
-//     };
-
-// // render() {
-
-// //     console.log(faces)
-
-// //     return(
-// //         <div className = "game-wrapper">
-// //             <div className = "tiles-div">
-// //                 {this.state.faces.map((keyName, i) => (
-// //                             <Tile 
-// //                                 Clicked={this.state.Clicked}
-// //                                 handleClick={this.handleClick}
-// //                                 id={keyName.id}
-// //                                 key={i}
-// //                                 image={keyName.image}
-// //                             />
-// //                 ))}
-// //             </div>
-// //         </div>
-// //     )           
-// // };
-
-// }
-
-
-// export default Game;
--- src/components/Tile/index.js
+++ src/components/Tile/index.js
@@ -1,76 +1,64 @@
-// import React, {Component} from 'react';
-
-import React, {useState} from 'react';
-import './styles.css';
-import faces from '../Faces/index.js';
-import controller from '../../img/controller.png';
-// import donkeykong from '../../img/donkeykong.jpg';
-// import mario from '../../img/mario.jpg';
-// import banjo from '../../img/banjo.jpg';
-// import zelda from '../../img/zelda.png';
-// import diddykong from '../../img/diddykong.jpg';
-// import sonic from '../../img/sonic.jpg';
-// import pacman from '../../img/pacman.png';
-// import luigi from '../../img/luigi.jpg';
-// import yoshi from '../../img/yoshi.jpg';
-// import link from '../../img/link.jpg';
-
-// let characters =  [donkeykong, diddykong, mario, luigi, yoshi, banjo, pacman, zelda, link, sonic];
-
-// let characters = faces;
+// import React, {useState} from 'react';
+// import './styles.css';
+// import faces from '../Faces/index.js';
+// import controller from '../../img/controller.png';
 
 
-// let imagesPath = {
-//     up: characters[0],
-//     down: controller
-//   }
-  
-//   class Tile extends Component {
-//     state = {
-//       faceDown: true
+// function Tile(card) {
+
+//     let characters = faces;
+
+//     let index = card.id;
+
+//     let imagesPath = {
+//         up: characters[index],
+//         down: controller
 //     }
-//     toggleImage = () => {
-//       this.setState(state => ({ faceDown: !state.faceDown }))
-//     }
+
+//     // let flippedTiles = [];
+//     // let numMatches;
+
+//     let [faceDown, setFaceDown] = useState(true);
+//     // let [flippedTiles, setFlippedTiles] = useState(0);
+//     // let [isMatch, updateMatch] = useState(false);
+
+//       let cardClick = () => {
+//         setFaceDown(!faceDown);
+//         // console.log(faces[index]);
+//         // if (flippedTiles.length < 2 && faceDown === false) {
+//         //   setFlippedTiles(faces[index]);
+//         //   // console.log(flippedTiles);
+//         // }
+
+//         //   if (flippedTiles.length === 2) {
+//         //       if (flippedTiles[0] === flippedTiles[1]) {
+//         //           flippedTiles[0].isMatch = true;
+//         //           flippedTiles[1].isMatch = true;
+//         //           flippedTiles.length = 0;
+//         //           numMatches++;
+//         //           console.log(numMatches);
+//         //       }
+//         //   }
+        
+//       }
+
+
+
+//     let getImageName = () => faceDown ? 'down' : 'up';
+
+//     const imageName = getImageName();
+    
+
+//       return (
+//         <div>
+//             <div className="tile-wrapper" onClick={cardClick}>
+//                 <img src={imagesPath[imageName]} className = "tile-img" alt = "card" id = {card.id}/>
+//             </div>
+//         </div>
+//       );
+//     };
   
-//     getImageName = () => this.state.faceDown ? 'down' : 'up'
-
-function Tile(card) {
-
-let characters = faces;
-
-let index = card.id;
-
-console.log(index);
-
-let imagesPath = {
-    up: characters[index],
-    down: controller
-  }
-
-  let [faceDown, setFaceDown] = useState(true);
-
-      let toggleImage = () => {
-        setFaceDown(!faceDown);
-    }
-
-    let getImageName = () => faceDown ? 'down' : 'up';
-
-    const imageName = getImageName();
-
-    // render() {
-    //   const imageName = this.getImageName();
-      return (
-        <div>
-            <div className="tile-wrapper">
-                <img src={imagesPath[imageName]} onClick={toggleImage} className = "tile-img" alt = "card" id = {card.id}/>
-            </div>
-        </div>
-      );
-    }
-//   }
-  
-export default Tile;
+// export default Tile;
 
 
 // import { useState } from 'react';
@@ -157,18 +145,22 @@ export default Tile;
 // }
 
 // export default Tile;
-// import React from 'react';
-// import './styles.css';
 
-// function Tile(props) {
-//     return(
-//         <div className="tile-wrapper" onClick={(e) => props.handleClick(e, props.id)}>
-//             <div className="img-div">
-//                 <img src = {props.image} alt = "tile" className = "tile-img"/>
-//             </div>
-//         </div>
-//     )
-// }
+import React from 'react';
+import './styles.css';
 
-// export default Tile;
+function Tile(props) {
+
+    let i = props.id;
+  
+    return(
+        <div className="tile-wrapper">
+            <div className="img-div">
+                <img src = {props.image} onClick={() => props.cardClick(i)} alt = "tile" className = "tile-img"/>
+            </div>
+        </div>
+    )
+}
+
+export default Tile;
 
